8fd1a4b45a7e38ff2690499feed5e30edbffe4e1
Goeun <wjdrhdms002@naver.com>
2020-11-16 17:21:55 +0900

    20201116 | Add DiningInfoCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as DiningPaper } from './DiningPaper';
 export { default as DiningList } from './DiningList';
+export { default as DiningInfoCard} from './DiningInfoCard'
 export { default as AppContainer } from './AppContainer';
 export { default as Map } from './Map';
